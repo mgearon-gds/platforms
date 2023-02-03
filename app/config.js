c3c8fcc80755fb1eabb9c9970f892d54107c33a1
mgearon-gds <103048725+mgearon-gds@users.noreply.github.com>
2023-02-03 13:00:19 +0000

    Changes

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Build a service',
+  serviceName: 'Platforms',
 
   // Default port that prototype runs on
   port: '3000',
