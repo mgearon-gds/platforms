f5196b8fe7f8891d37de4a01729d41a839536bb0
mgearon-gds <103048725+mgearon-gds@users.noreply.github.com>
2023-06-08 10:39:06 +0100

    Changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,5 +4,33 @@
 //
 
 window.GOVUKPrototypeKit.documentReady(() => {
-  // Add JavaScript here
+  function hideNotificationBanner() {
+    var notificationBanner= document.querySelector('.govuk-insight');
+    notificationBanner.style.display = 'none';
+
+    var expiryDate = new Date();
+    expiryDate.setTime(expiryDate.getTime() + (60 * 60 * 1000));
+    document.cookie = 'hideNotificationBanner=true; expires=' + expiryDate.toUTCString() + '; path=/';
+  }
+
+  function checkNotificationCookie() {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i< cookies.length; i++){
+      var cookie = cookies[i].trim();
+      if (cookie.indexOf('hideNotificationBanner=true') === 0){
+        hideNotificationBanner();
+        break;
+      }
+    }
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    checkNotificationCookie();
+  });
+
+  var linkElement = document.querySelector('#hide-insight');
+  linkElement.addEventListener('click', function(event){
+    event.preventDefault();
+    hideNotificationBanner();
+  });
 })
